b33f53eb3fa6015f4c5afeb35314e264d5851a8b
henry <tarou.88@gmail.com>
2020-03-15 20:38:18 +0900

    set Register as initial Route

---
+++ src/Center.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import {View} from 'react-native';
+
+interface CenterProps {}
+
+export const Center: React.FC<CenterProps> = ({children}) => {
+  return (
+    <View
+      // eslint-disable-next-line react-native/no-inline-styles
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      {children}
+    </View>
+  );
+};
--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,24 +1,34 @@
 import * as React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
-import {View, Text} from 'react-native';
+import {Text} from 'react-native';
+import {Center} from './Center';
 interface RoutesProps {}
 
 const Stack = createStackNavigator();
 
 function Login() {
   return (
-    <View>
+    <Center>
       <Text>Login Screen</Text>
-    </View>
+    </Center>
+  );
+}
+
+function Register() {
+  return (
+    <Center>
+      <Text>Register Screen</Text>
+    </Center>
   );
 }
 
 export const Routes: React.FC<RoutesProps> = ({}) => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Register">
         <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register} />
       </Stack.Navigator>
     </NavigationContainer>
   );
