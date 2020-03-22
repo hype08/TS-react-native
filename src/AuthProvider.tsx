e923567ae4f1a92a39ba47ef606535cc73498162
henry <tarou.88@gmail.com>
2020-03-22 21:56:40 +0900

    add type and defaults to AuthContext

--- src/AuthProvider.tsx
+++ src/AuthProvider.tsx
@@ -1,12 +1,22 @@
 import React, {useState} from 'react';
 import {AsyncStorage} from 'react-native';
 
-export const AuthContext = React.createContext({});
+type User = null | {username: string};
+
+export const AuthContext = React.createContext<{
+  user: User;
+  login: () => void;
+  logout: () => void;
+}>({
+  user: null,
+  login: () => {},
+  logout: () => {},
+});
 
 interface AuthProviderProps {}
 
 export const AuthProvider: React.FC<AuthProviderProps> = ({children}) => {
-  const [user, setUser] = useState<{username: string} | null>(null);
+  const [user, setUser] = useState<User>(null);
   return (
     <AuthContext.Provider
       value={{
